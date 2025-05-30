50d16c7ed39b95a3173bcac96d45ce5406961102
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-05-30 09:58:34 +0530

    added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
     }
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/auth/login`,
+        "https://emplyeepanel.vercel.app/api/auth/login",
         { username: f_userName, password: f_Pwd },
         { withCredentials: true }
       );
